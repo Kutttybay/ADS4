e3155c6040ad0649091e172eefb80297f246894c
kutttybay <kutttybay@gmail.com>
2023-05-14 15:28:21 +0600

    3 commit

--- src/HashNode.java
+++ src/HashNode.java
@@ -6,4 +6,15 @@ public class HashNode<K, V> {
         this.value = value;
     }
 
+    public K getKey(){
+        return key;
+    }
+
+    public V getValue(){
+        return value;
+    }
+
+    public void setValue(V value){
+        this.value = value;
+    }
 }
