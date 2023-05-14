9d95b5d7c4652b78913b74f83179a80ac363cc83
kutttybay <kutttybay@gmail.com>
2023-05-14 20:18:05 +0600

    9 commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello to Assignment 4");
+        HashTable hashTable = new HashTable<TestingClass, String>();
+        Random r = new Random();
+        // Create numbers by random
+        for (int i = 0; i < 10000; i++){
+            int val = r.nextInt(0, 1000000);
+            hashTable.put(new TestingClass(val), "Student " + val);
+        }
     }
 }
+
+// Class for testing
 class TestingClass{
     private int val;
     public TestingClass(int v){
